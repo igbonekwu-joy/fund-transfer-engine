a7885074aa86d78a04bc33593d7f2c42cd63872e
Joy <joykenneth605@gmail.com>
2026-07-28 18:28:25 +0100

    fix: add return type to allowed origins for cors

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_origins' => array_filter(array_map(
         'trim',
-        explode(',', env('CORS_ALLOWED_ORIGINS', ''))
+        explode(',', (string) env('CORS_ALLOWED_ORIGINS', ''))
     )),
 
     'allowed_origins_patterns' => [],
